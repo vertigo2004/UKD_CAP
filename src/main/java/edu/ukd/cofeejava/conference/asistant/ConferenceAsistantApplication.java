023ee53aa6fa9909e2b2841a798938b5886ac503
oleg.pashkevych <oleg.pashkevych@ryan.com>
2024-02-12 14:09:05 +0200

    Initial commit.

---
+++ src/main/java/edu/ukd/cofeejava/conference/asistant/ConferenceAsistantApplication.java
@@ -0,0 +1,13 @@
+package edu.ukd.cofeejava.conference.asistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceAsistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceAsistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ukd/cofeejava/conference/asistant/ConferenceAsistantApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ukd.cofeejava.conference.asistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceAsistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
